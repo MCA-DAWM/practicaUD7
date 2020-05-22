a8bc24c199897830691b3d098bfe202ecc333a6f
María Cremades Asensi <cremades20350@iesmarenostrum.com>
2020-05-22 17:13:55 +0200

    Movil

--- p7/Form1.Designer.cs
+++ p7/Form1.Designer.cs
@@ -102,9 +102,9 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(710, 29);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Teléfono:";
+            this.label3.Text = "Móvil:";
             // 
             // label4
             // 
